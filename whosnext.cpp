4cf621394318249904bac80f0ad1cedc779bd67f
Jaufré Lallement <lallement.j.pro@outlook.fr>
2022-12-23 16:50:23 +0100

    Edit: comments

--- whosnext.cpp
+++ whosnext.cpp
@@ -10,6 +10,14 @@
 
 using namespace std;
 
+/**
+ * @brief Checks if an integer is present in the given integer vector
+ *
+ * @param arr : vector of integers
+ * @param value : value to check
+ * @return true : the value is present
+ * @return false : the value is not present
+ */
 bool contains(vector<int> arr, int value)
 {
   for (int i = 0; i < arr.size(); i++)
@@ -22,6 +30,13 @@ bool contains(vector<int> arr, int value)
   return false;
 }
 
+/**
+ * @brief Returns the difference between 2 integer vectors
+ *
+ * @param a : first vector
+ * @param b : second vector
+ * @return vector<int> : difference between vector a and vector b
+ */
 vector<int> indexDiff(vector<int> a, vector<int> b)
 {
   vector<int> diff;
@@ -37,6 +52,13 @@ vector<int> indexDiff(vector<int> a, vector<int> b)
   return diff;
 }
 
+/**
+ * @brief Splits a string based on a given delimiter
+ *
+ * @param str : string to split
+ * @param delimiter : character to use as delimiter
+ * @return vector<string> : splitted string
+ */
 vector<string> split(const string &str, char delimiter)
 {
   vector<string> tokens;
@@ -49,6 +71,13 @@ vector<string> split(const string &str, char delimiter)
   return tokens;
 }
 
+/**
+ * @brief Assign indexes randomly based on available indexes
+ *
+ * @param rankedPeople : vector of people with already assigned indexes
+ * @param unrankedPeople : vector of people without their index assigned
+ * @param places : available indexes to assign
+ */
 void order(vector<Person> &rankedPeople, vector<Person> &unrankedPeople, vector<int> places)
 {
   srand(time(0));
@@ -64,6 +93,11 @@ void order(vector<Person> &rankedPeople, vector<Person> &unrankedPeople, vector<
        { return p1.getIndex() < p2.getIndex(); });
 }
 
+/**
+ * @brief Read the lines of a file and print them based on a computed order
+ *
+ * @param filename : path to the file to read
+ */
 void readLines(const char *filename)
 {
   ifstream file(filename);
@@ -77,6 +111,8 @@ void readLines(const char *filename)
   {
 
     Person p;
+
+    // If the line contains a comma : we assign name and index to the person
     if (s.find(',') != string::npos)
     {
       vector<string> splitLine = split(s, ',');
@@ -87,15 +123,18 @@ void readLines(const char *filename)
       ranked.push_back(p);
       usedIndexes.push_back(index);
     }
+    // If not: we only assign the name
     else
     {
       p.setName(s);
       unranked.push_back(p);
     }
+
     indexes.push_back(i);
     i++;
   }
 
+  // Order names then print the result
   order(ranked, unranked, indexDiff(indexes, usedIndexes));
   for (Person p : ranked)
   {
@@ -103,6 +142,13 @@ void readLines(const char *filename)
   }
 }
 
+/**
+ * @brief Main entry point
+ *
+ * @param argc
+ * @param argv function arguments
+ * @return int
+ */
 int main(int argc, char *argv[])
 {
   const char *filename = argv[1];
